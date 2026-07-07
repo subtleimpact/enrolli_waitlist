c6a278802e8668230e182312cc91a98d8d07f98c
subtleimpact <subtleimpact@outlook.com>
2026-07-06 22:14:13 -0500

    Delete enrolli-waitlist directory

--- enrolli-waitlist/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Waitlist from "./Waitlist.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Waitlist />
-  </React.StrictMode>
-);
